b359310ede0e77de06354daad87cb8da6c8ee879
kori756 <khorirachmatr@gmail.com>
2026-05-14 22:29:13 +0700

    menyelesaikan prosedur

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -35,4 +35,16 @@ public:
     }
 
      void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+        delete temp;
+    }
+
     
