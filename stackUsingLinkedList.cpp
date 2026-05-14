1b3a3a18c6cd9c8e5f0cd46fa9312685cc8b2a53
kori756 <khorirachmatr@gmail.com>
2026-05-14 22:28:01 +0700

    membuat member push di public

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -19,4 +19,19 @@ class stack
 private:
     Node *top;
 public:
-    
+    stack()
+    {
+        top = NULL;
+    }
+
+     int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+     
